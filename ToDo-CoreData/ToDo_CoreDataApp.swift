10246c60189eee2fd2ce2a632110916dafe136bb
Saurabh Jaiswal <sjsaurabhjais@gmail.com>
2024-12-31 10:50:16 +0530

    Initial Commit

---
+++ ToDo-CoreData/ToDo_CoreDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDo_CoreDataApp.swift
+//  ToDo-CoreData
+//
+//  Created by Saurabh Jaiswal on 31/12/24.
+//
+
+import SwiftUI
+
+@main
+struct ToDo_CoreDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
